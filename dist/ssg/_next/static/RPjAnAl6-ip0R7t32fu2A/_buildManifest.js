45dc4a825123eb633f8f5891db5a1505e8bd87c6
Uindow <181064963+uindow@users.noreply.github.com>
2026-03-15 15:15:00 +0200

    [release] Version 0.0.30
    # March 15, 2026
    [code] Improved CPU usage in pilot mode
    [code] Updated subscription dialog
    [code] Improved subscription dialog
    [code] Toolbar locked dialog
    [code] Added affiliate program link
    
    # March 14, 2026
    [code] Improved animation UX
    [code] Improved animation transitions
    [code] Added free forever plan
    [chore] Linting
    
    # March 13, 2026
    [code] Added history controls
    
    # March 12, 2026
    [code] Updated app-update.yml details

---
+++ dist/ssg/_next/static/RPjAnAl6-ip0R7t32fu2A/_buildManifest.js
@@ -0,0 +1,17 @@
+/*!
+ * @architect Mark Jivko <mark@uindow.com>
+ * @copyright © 2026 Uindow (https://uindow.com)
+ * 
+ * Licensed under the Uindow™ Source-Available License, Version 1.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     https://github.com/uindow/dos/blob/main/LICENSE.txt
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+self.__BUILD_MANIFEST=function(s,e,t,r){return{__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},__routerFilterStatic:{numItems:0,errorRate:1e-4,numBits:0,numHashes:null,bitArray:[]},__routerFilterDynamic:{numItems:s,errorRate:1e-4,numBits:s,numHashes:null,bitArray:[]},"/":[r,"static/chunks/559-6ac57e2d0f248fff.js","static/chunks/pages/index-e7c9aa41ee7c30c6.js"],"/404":["static/chunks/pages/404-dc2de4c822b4de18.js"],"/500":["static/chunks/pages/500-246f58f998d0243b.js"],"/503":["static/chunks/pages/503-200678112ef9a24b.js"],"/_error":["static/chunks/pages/_error-c99528965161d4de.js"],"/login":[r,"static/chunks/474-91ba44fc66cc698a.js","static/chunks/pages/login-9ee9d7d4d8d11f5b.js"],sortedPages:["/","/404","/500","/503","/_app","/_error","/login"]}}(0,1e-4,null,"static/chunks/931-22507f9a5f02fc43.js"),self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
